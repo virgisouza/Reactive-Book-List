7e1f77037d11ae7699da9ac3c799099fd96d0de5
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-10-30 17:01:00 -1000

    initial set up of components and containers to receive data, need to add methods to class

---
+++ src/components/bookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import booksFromFakeDB from './lib/books.db';
+
+const BookList = (props) => {
+
+
+  return (
+
+  );
+};
+
+
+export default BookList;
---
+++ src/containers/newBookForm.js/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class NewBookForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bookInput: ''
+    }
+  }
+
+  //add methods
+  render(){
+
+    return (
+      <div></div>
+
+    )
+  }
+
+
+};
+
+export default NewBookForm;
